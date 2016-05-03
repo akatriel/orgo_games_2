697b808604d83ca2545ffbb0c34ec9a2a15f1bbd
akatriel <michael.c.gabay@gmail.com>
2016-05-03 18:00:24 -0400

    set up file structure for games.

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,10 @@
+class GamesController < ApplicationController
+	def alkanes
+	end
+	def alkenes
+	end
+	def chairs
+	end
+	def newmans
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   root 'home#index'
+  get 'alkanes' => 'games#alkanes'
+  get 'alkenes' => 'games#alkenes'
+  get 'chairs' => 'games#chairs'
+  get 'newmans' => 'games#newmans'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 

46a6a889f5b91f59d2a49ff7e4a9838d2a6e2b3d
akatriel <michael.c.gabay@gmail.com>
2016-05-04 16:40:13 -0400

    set up alkanes except ajax.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,12 @@
 class GamesController < ApplicationController
 	def alkanes
+		alkanes = ['methane', 'ethane', 'propane', 'butane', 'pentane', 'hexane', 'heptane', 'octane', 'nonane', 'decane']
+		@alkane = alkanes[rand(10)]
+		@alkane unless nil
+		respond_to do |format|
+			format.html
+			format.js
+		end
 	end
 	def alkenes
 	end
